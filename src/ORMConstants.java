1377be3c1cd4c247fd80b2609107ed9ed5bd6c8c
Miwysocki <34211963+Miwysocki@users.noreply.github.com>
2021-06-15 15:15:55 +0200

    Initial commit

--- src/ORMConstants.java
+++ src/ORMConstants.java
@@ -1,16 +1,3 @@
-/**
- * "Visual Paradigm: DO NOT MODIFY THIS FILE!"
- * 
- * This is an automatic generated file. It will be regenerated every time 
- * you generate persistence class.
- * 
- * Modifying its content may cause the program not work, or your work may lost.
- */
-
-/**
- * Licensee: 
- * License Type: Evaluation
- */
 public interface ORMConstants extends org.orm.util.ORMBaseConstants {
 	final int KEY_CUSTOMER_DELIVERYADDRESS = 1844902687;
 	
